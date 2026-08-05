3b077e15e92b1a75281fd5a860adb31bbc3497fd
Asim Malik <asimmalik1105@example.com>
2026-08-05 23:39:18 +0530

    chore: init backend and frontend scaffold

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id);
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
+
+server.listen(4000, () => console.log('server running on 4000'));
